c04fdf70235d0789d3439ba73cd828be0309e6f0
Austin Pachano (SonosFuer) <pachano.austin@gmail.com>
2018-03-21 21:42:58 -0700

    Now bullet uses universal entity that is assigned properties apon firing

--- functions.lua
+++ functions.lua
@@ -1,3 +1,68 @@
+guns.ammo = {}
+
+--This probably needs moved to a new file
+--Technically is not a function but it is nessacary
+--for the functions to work
+
+minetest.register_entity("guns:bullet", {
+	physical = false,
+	visual_sie = "sprite",
+	visual_size =  {x = 0.5, y = 0.5},
+	textures = {"bullet"},
+	collisionbox = {0, 0, 0, 0, 0, 0},
+	timer = 0,
+	switch = 0,
+
+	onstep = function(self)
+		local pos = self.object:get_pos()
+
+		-- If the bullet hits a node do this
+		if self.hit_node then
+			local node = node_ok(pos).name
+
+			if minetest.registered_nodes[node].walkable then
+				self.hit_node(self, pos, node)
+
+				if self.drop == true then
+					pos.y = pos.y + 1
+					self.lastpos = (self.lastpos or pos)
+					minetest.add_item(self.lastpos, self.object:get_luaentity().name)
+				end
+
+				self.object:remove() ; -- print ("hit node")
+				return
+			end
+		end
+
+
+		-- If the bullet hits a player or a mob do this
+		if self.hit_player or self.hit_mob then
+			for _,player in pairs(minetest.get_objects_inside_radius(pos, 1.0)) do
+				if self.hit_player
+				and player:is_player() then
+					self.hit_player(self, player)
+					self.object:remove() ; -- print ("hit player")
+					return
+				end
+
+				local entity = player:get_luaentity()
+
+				if entity
+				and self.hit_mob
+				and entity._cmi_is_mob == true
+				and tostring(player) ~= self.owner_id
+				and entity.name ~= self.object:get_luaentity().name then
+					self.hit_mob(self, player)
+					self.object:remove() ;  --print ("hit mob")
+					return
+				end
+			end
+		end
+
+		self.lastpos = pos
+	end
+})
+
 function guns.register_ammo(modname, ammo)
 	-- Information on ammo object
 	-- name -> name of ammo eg: 22cal_incendiary
@@ -10,6 +75,8 @@ function guns.register_ammo(modname, ammo)
 	-- entity.hit_player -> Function called when hits player, parameters are (self, player)
 	-- entity.hit_node -> Function called when hits node, parameters are (self, pos, node)
 
+	if not ammo.entity.hit_mob then ammo.entity.hit_mob = ammo.entity.hit_player end
+
 	minetest.register_craftitem(modname .. ":" .. ammo.name, {
 		description = ammo.name .. " ammo",
 		groups = {ammo = 1},
@@ -20,70 +87,8 @@ function guns.register_ammo(modname, ammo)
 		--liquids_pointable
 	})
 
-	if not ammo.entity.hit_mob then ammo.entity.hit_mob = ammo.entity.hit_player end
+	guns.ammo[modname .. ":" .. ammo.name] = ammo
 
-	minetest.register_entity(modname .. ":" .. ammo.name .. "_entity",{
-		physical = false,
-		visual = ammo.entity.visual,
-		visual_size = ammo.entity.visual_size,
-		textures = ammo.entity.textures,
-		hit_player = ammo.entity.hit_player,
-		hit_mob = ammo.entity.hit_mob,
-		hit_node = ammo.entity.hit_node,
-		collisionbox = {0, 0, 0, 0, 0, 0},
-		timer = 0,
-		switch = 0,
-		owner_id = ammo.entity.owner_id,
-
-		onstep = function(self)
-			local pos = self.object:get_pos()
-
-			-- If the bullet hits a node do this
-			if self.hit_node then
-				local node = node_ok(pos).name
-
-				if minetest.registered_nodes[node].walkable then
-					self.hit_node(self, pos, node)
-
-					if self.drop == true then
-						pos.y = pos.y + 1
-						self.lastpos = (self.lastpos or pos)
-						minetest.add_item(self.lastpos, self.object:get_luaentity().name)
-					end
-
-					self.object:remove() ; -- print ("hit node")
-					return
-				end
-			end
-
-
-			-- If the bullet hits a player or a mob do this
-			if self.hit_player or self.hit_mob then
-				for _,player in pairs(minetest.get_objects_inside_radius(pos, 1.0)) do
-					if self.hit_player
-					and player:is_player() then
-						self.hit_player(self, player)
-						self.object:remove() ; -- print ("hit player")
-						return
-					end
-
-					local entity = player:get_luaentity()
-
-					if entity
-					and self.hit_mob
-					and entity._cmi_is_mob == true
-					and tostring(player) ~= self.owner_id
-					and entity.name ~= self.object:get_luaentity().name then
-						self.hit_mob(self, player)
-						self.object:remove() ;  --print ("hit mob")
-						return
-					end
-				end
-			end
-
-			self.lastpos = pos
-		end
-	})
 end
 
 function guns.register_part(modname, part)
@@ -132,6 +137,15 @@ function guns.register_gun(modname, gun)
 					local aim = user:get_look_dir()
 					local pos = user:get_pos()
 					local bullet = minetest.add_entity({x=pos.x,y=pos.y+1.5,z=pos.z}, gun.ammo .. "_entity")
+					local ammo = guns.ammo[gun.ammo]
+					bullet:set_properties({
+						visual = ammo.entity.visual,
+						visual_size = ammo.entity.visual_size,
+						textures = ammo.entity.textures,
+						hit_player = ammo.entity.hit_player,
+						hit_mob = ammo.entity.hit_mob,
+						hit_node = ammo.entity.hit_node
+					})
 					bullet:set_velocity({x=aim.x*19, y=aim.y*19, z=aim.z*19})
 				else
 					--Play empty noise
